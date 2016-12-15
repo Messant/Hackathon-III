3950f14f5fdc430eeb483cc65078bd680953782e
Jpilosel <formation@pilosel.fr>
2016-12-15 17:44:51 +0100

    initialisation de la partie

--- src/BackBundle/Controller/DefaultController.php
+++ src/BackBundle/Controller/DefaultController.php
@@ -4,6 +4,10 @@ namespace BackBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use BackBundle\Entity\User;
+use BackBundle\Form\UserType;
+use FOS\UserBundle\Form\Type\UsernameFormType;
+
 
 class DefaultController extends Controller
 {
@@ -13,5 +17,50 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('BackBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/go", name="go")
+     */
+    public function statusAction()
+    {
+        return $this->render('FrontBundle:Default:admin_debut.html.twig', array(
+            'couleur_etat' => $this->getStatus_couleur(),
+        ));
+    }
+
+    private function getStatus_couleur()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $etats = $em->getRepository('BackBundle:Etat')->findAll();
+        $etatsactu = $em->getRepository('FrontBundle:EtatJeux')->findAll();
+        $statut = $etatsactu[0]->getEtat() ;
+        $couleur =($etats [$statut]);
+        $couleur_etat =($couleur-> getCouleur());
+        return $couleur_etat;
+    }
+
+    /**
+     * @Route("/gogo", name="gogo")
+     */
+    public function getGo_etat()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $en_attente =$em->getRepository('FrontBundle:EtatJeux')->find(1);
+        $en_attente->setEtat(1);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($en_attente);
+        $em->flush();
+
+        $joueurs =$em->getRepository('BackBundle:User')->findAll();
+        $elu=$joueurs[array_rand($joueurs)];
+        $elu->setMeneur(1);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($elu);
+        $em->flush();
+
+        return $this->render('FrontBundle:Default:joueur.html.twig', array(
+            'couleur_etat' => $this->getStatus_couleur(),
+        ));
     }
 }
